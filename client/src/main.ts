6e382e71dc51d8b78bf78c23679b880b6bb331ed
maxellmilay <milaymaxell@gmail.com>
2023-08-08 11:54:55 +0800

    [Adjustment][Maxell] adjusted files for deployment

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
 import store from './store';
 import ToastPlugin from 'vue-toast-notification';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'https://milaymaxell.pythonanywhere.com';
 
 const app = createApp(App);
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-//@ts-ignore:next-line
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
-  },
   plugins: [vue()],
 });
 
